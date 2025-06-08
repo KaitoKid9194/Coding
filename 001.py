ea6ba59bdb38f971688dd25c56963b1053fc2cbd
KaitoKid9194 <W180002360@student.wass.edu.vn>
2025-06-08 08:14:10 +0700

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from sklearn.linear_model import LinearRegression
+import feedparser
+
+st.sidebar.title("🎶 Danh sách nghệ sĩ")
+selected_artist = st.sidebar.radio("Chọn nghệ sĩ:", ["Đen Vâu", "Hà Anh Tuấn", "Sơn Tùng M-TP"])
+
+videos = {
+    "Đen Vâu": [
+        ("Bữa ăn cho em", "https://www.youtube.com/watch?v=ukHK1GVyr0I"),
+        ("Mang tiền về cho mẹ", "https://www.youtube.com/watch?v=UVbv-PJXm14"),
+        ("Trời hôm nay nhiều mây cực!", "https://www.youtube.com/watch?v=MBaF0l-PcRY"),
+        ("Hai triệu năm", "https://www.youtube.com/watch?v=LSMDNL4n0kM")
+    ],
+    "Hà Anh Tuấn": [
+        ("Tuyết rơi mùa hè", "https://www.youtube.com/watch?v=pTh3KCD7Euc"),
+        ("Nước ngoài", "https://www.youtube.com/watch?v=pU3O9Lnp-Z0"),
+        ("Tháng tư là lời nói dối của em", "https://www.youtube.com/watch?v=UCXao7aTDQM"),
+        ("Xuân thì", "https://www.youtube.com/watch?v=3s1r_g_jXNs")
+    ],
+    "Sơn Tùng M-TP": [
+        ("Lạc trôi", "https://www.youtube.com/watch?v=Llw9Q6akRo4"),
+        ("Chúng ta không thuộc về nhau", "https://www.youtube.com/watch?v=qGRU3sRbaYw"),
+        ("Muộn rồi mà sao còn", "https://www.youtube.com/watch?v=xypzmu5mMPY"),
+        ("Hãy trao cho anh", "https://www.youtube.com/watch?v=knW7-x7Y7RE")
+    ]
+}
+
+st.title("🎧 Ứng dụng giải trí và sức khỏe")
+
+tab1, tab2, tab3 = st.tabs(["🎤 MV yêu thích", "💤 Dự đoán giờ ngủ", "📰 Đọc báo" ])
+
+with tab1:
+    st.header(f"Các bài hát của {selected_artist} 🎵")
+    for title, url in videos[selected_artist]:
+        st.subheader(title)
+        st.video(url)
